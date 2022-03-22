9ed71a612edf6b705b03a03d877a8af05ba81f6c
antonikowalik <92799829+antonikowalik@users.noreply.github.com>
2022-03-22 13:02:19 +0100

    2022-03-22 AK

--- scraper.py
+++ scraper.py
@@ -1,7 +1,11 @@
-from urllib import response
+from bs4 import BeautifulSoup
 import requests
 
 url = "https://www.booking.com/hotel/pl/happy-tower.html"
 response = requests.get(url)
-print(response.status_code)
-print(response.text)
+
+page_dom = BeautifulSoup(response.text, 'html.parser')
+
+reviews = page_dom.select("script[data-capla-store-sata='apollo']")
+review = reviews.pop(-1)
+print(review)
